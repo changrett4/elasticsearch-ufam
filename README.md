0343a77f829bec5c71c7dd2b47466535e424ca26
Alexandre da Fonseca Vieira <107149857+SnipAlex@users.noreply.github.com>
2024-06-15 12:08:00 -0400

    Adicionado novo readme e elastic

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# elasticsearch-ufam
+# Elastic-basic
+Primeiro é preciso rodar o docker do elastic sem a necessidade de credenciais
+```bash
+docker run --rm -p 9200:9200 -p 9300:9300 -e "xpack.security.enabled=false" -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:8.7.0
+```
+Na pasta server instale as dependências
+```bash
+python3 -m pip install pandas==1.4.3 notebook==6.3.0 elasticsearch==8.7.0
+```
+
+Ainda na pasta server copie o arquivo amazon_products.csv
+
+
+Rode o arquivo elastic.py
+```bash
+python3 elastic.py
+```
+
+Teste uma consulta no servidor. No seu navegador coloque essa rota
+```bash
+http://localhost:5000/search?q=smart%20TV
+```
---
+++ elastic.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, jsonify
+from elasticsearch import Elasticsearch
+import pandas as pd
+from elasticsearch.helpers import bulk
+
+app = Flask(__name__)
+
+# Conectar ao Elasticsearch
+es = Elasticsearch("http://localhost:9200")
+
+# Leitura do arquivo CSV
+df = pd.read_csv("amazon_products.csv")
+
+# Remoção de linhas com valores ausentes
+df = df.dropna()
+
+# Obter o número de linhas do DataFrame
+num_rows = df.shape[0]
+
+# Definir o tamanho da amostra como o menor valor entre 10000 e o total de linhas
+sample_size = min(10000, num_rows)
+
+# Amostragem do DataFrame e reset dos índices
+df = df.sample(sample_size, random_state=42).reset_index(drop=True)
+
+# Inserção de dados no Elasticsearch
+bulk_data = []
+for i, row in df.iterrows():
+    bulk_data.append(
+        {
+            "_index": "products",
+            "_id": i,
+            "_source": {        
+                "title": row["title"],
+            }
+        }
+    )
+bulk(es, bulk_data)
+
+# Refresh índice para garantir que todos os documentos sejam pesquisáveis
+es.indices.refresh(index="products")
+
+@app.route('/search', methods=['GET'])
+def search():
+    # Obtendo o parâmetro de busca da URL
+    query = request.args.get('q', '')
+    
+    # Realizando a busca no Elasticsearch
+    resp = es.search(
+        index="products",
+        query={
+            "bool": {
+                "must": {
+                    "match": {
+                        "title": query,
+                    }
+                },
+            },
+        },
+        size=10  
+    )
+    
+    # Formatando o resultado
+    results = [hit['_source'] for hit in resp['hits']['hits']]
+    
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
